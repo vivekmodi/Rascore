3478770db138abf1395af2f26eff2857324f438d
Vivek Modi <modi.vivek2009@gmail.com>
2021-04-14 17:10:00 -0400

    Modified PDB summary table

--- Rascore.py
+++ Rascore.py
@@ -11,14 +11,6 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-#from flask_wtf import FlaskForm
-#from wtforms import StringField, SubmitField
-#from wtforms.validators import DataRequired
-#from werkzeug.utils import secure_filename
-#from sqlalchemy import func
-#from collections import defaultdict
-#from flask import Markup
-#from sqlalchemy import desc,asc
 import pandas as pd
 from natsort import natsorted
 import pickle
@@ -44,8 +36,10 @@ Migrate(app,db)
 
 class Clusters(db.Model):
     pdb=db.Column(db.Text,primary_key=True)
+    gene_name=db.Column(db.Text)
     protein_name=db.Column(db.Text)
     bound_protein=db.Column(db.Text)
+    bound_peptide=db.Column(db.Text)
     mutation_status=db.Column(db.Text)
     nucleotide_class=db.Column(db.Text)
     drug_class=db.Column(db.Text)
@@ -54,19 +48,35 @@ class Clusters(db.Model):
     switch1_cluster=db.Column(db.Text)
     switch2_cluster=db.Column(db.Text)
     conformational_state=db.Column(db.Text)
+    residue_range=db.Column(db.Text)
+    nucleotide=db.Column(db.Text)
+    drug=db.Column(db.Text)
+    other_ligands=db.Column(db.Text)
+    experiment_type=db.Column(db.Text)
+    resolution=db.Column(db.Text)
+    rfactor=db.Column(db.Text)
+    crystal_form=db.Column(db.Text)
+    deposit_date=db.Column(db.Text)
+    pmid=db.Column(db.Text)
 
     
-    def __init__(self,pdb=pdb,protein_name=protein_name,bound_protein=bound_protein,mutation_status=mutation_status,nucleotide_class=nucleotide_class,drug_class=drug_class,\
+    def __init__(self,pdb=pdb,gene_name=gene_name,protein_name=protein_name,bound_protein=bound_protein,bound_peptide=bound_peptide,mutation_status=mutation_status,nucleotide_class=nucleotide_class,drug_class=drug_class,\
                  bound_protein_class=bound_protein_class,homodimer_status=homodimer_status,switch1_cluster=switch1_cluster,\
-                 switch2_cluster=switch2_cluster,conformational_state=conformational_state):
+                 switch2_cluster=switch2_cluster,conformational_state=conformational_state,residue_range=residue_range,nucleotide=nucleotide,\
+                 drug=drug,other_ligands=other_ligands,experiment_type=experiment_type,resolution=resolution,rfactor=rfactor,\
+                 crystal_form=crystal_form,deposit_date=deposit_date,pmid=pmid):
 
-        self.pdb=pdb;self.protein_name=protein_name;self.bound_protein=bound_protein;self.mutation_status=mutation_status;self.nucleotide_class=nucleotide_class;
+        self.pdb=pdb;self.gene_name=gene_name;self.protein_name=protein_name;self.bound_protein=bound_protein;self.bound_peptide=bound_peptide;
+        self.mutation_status=mutation_status;self.nucleotide_class=nucleotide_class;
         self.drug_class=drug_class;self.bound_protein_class=bound_protein_class;self.homodimer_status=homodimer_status;
         self.switch1_cluster=switch1_cluster;self.switch2_cluster=switch2_cluster;self.conformational_state=conformational_state;
+        self.residue_range=residue_range;self.nucleotide=nucleotide;self.drug=drug;self.other_ligands=other_ligands;self.experiment_type=experiment_type;
+        self.resolution=resolution;self.rfactor=rfactor;self.crystal_form=crystal_form;self.deposit_date=deposit_date;self.pmid=pmid;
 
     def __repr__(self):
-        return f'{self.pdb} {self.protein_name} {self.bound_protein} {self.mutation_status} {self.nucleotide_class} {self.drug_class} {self.bound_protein_class}\
-            {self.homodimer_status} {self.switch1_cluster} {self.switch2_cluster} {self.conformational_state}'
+        return f'{self.pdb} {self.gene_name} {self.protein_name} {self.bound_protein} {self.bound_peptide} {self.mutation_status} {self.nucleotide_class} {self.drug_class} {self.bound_protein_class}\
+            {self.homodimer_status} {self.switch1_cluster} {self.switch2_cluster} {self.conformational_state} {self.residue_range} {self.nucleotide}\
+            {self.drug} {self.other_ligands} {self.experiment_type} {self.resolution} {self.rfactor} {self.crystal_form} {self.deposit_date} {self.pmid}'
 
 def create_lists():
     pdb_set=set();conformation_set=set();bound_protein_set=set();bound_protein_class_set=set();nucleotide_class_set=set();drug_class_set=set()
@@ -241,9 +251,18 @@ def uniqueQuery(settings,queryname):
         if len(queryname)==5:
             queryname=queryname[0:4]           #Remove chain so that only PDB id is always searched
         pdb_list=Clusters.query.filter(Clusters.pdb.contains(queryname)).all()
-        pdb_count=Clusters.query.filter(Clusters.pdb.contains(queryname)).count()
-       
-        return render_template('pdbs.html',queryname=queryname,pdb_list=pdb_list,pdb_count=pdb_count)
+        protein_name=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().protein_name
+        experiment_type=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().experiment_type
+        resolution=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().resolution
+        rfactor=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().rfactor
+        crystal_form=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().crystal_form
+        homodimer_status=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().homodimer_status
+        residue_range=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().residue_range
+        pmid=Clusters.query.filter(Clusters.pdb.contains(queryname)).first().pmid
+        
+        return render_template('pdbs.html',queryname=queryname,pdb_list=pdb_list,protein_name=protein_name,\
+                               experiment_type=experiment_type,resolution=resolution,rfactor=rfactor,crystal_form=crystal_form,\
+                               homodimer_status=homodimer_status,residue_range=residue_range,pmid=pmid)
 
     if settings=='All':
         retrieve_str=dict();chain_count=dict();entry_count=dict()
--- update-database.py
+++ update-database.py
@@ -18,8 +18,10 @@ def update_database(df):
     db.create_all()
     for i in df.index:
         ras_clus=Clusters(pdb=df.at[i,'PDB ID'],\
+        gene_name=df.at[i,'Gene Name'],\
         protein_name=df.at[i,'Protein Name'],\
         bound_protein=df.at[i,'Bound Protein'],\
+        bound_peptide=df.at[i,'Bound Peptide'],\
         mutation_status=df.at[i,'Mutation Status'],\
         nucleotide_class=df.at[i,'Nucleotide Class'],\
         drug_class=df.at[i,'Drug Class'],\
@@ -27,50 +29,60 @@ def update_database(df):
         homodimer_status=df.at[i,'Homodimer Status'],\
         switch1_cluster=df.at[i,'Switch 1 Cluster'],\
         switch2_cluster=df.at[i,'Switch 2 Cluster'],\
-        conformational_state=df.at[i,'Conformational State'])
+        conformational_state=df.at[i,'Conformational State'],\
+        residue_range=df.at[i,'Residue Range'],\
+        nucleotide=df.at[i,'Nucleotide'],\
+        drug=df.at[i,'Drug'],\
+        other_ligands=df.at[i,'Other Ligands'],\
+        experiment_type=df.at[i,'Experiment Type'],\
+        resolution=df.at[i,'Resolution'],\
+        rfactor=df.at[i,'R-Factor'],\
+        crystal_form=df.at[i,'Crystal Form'],\
+        deposit_date=df.at[i,'Deposit Date'],\
+        pmid=df.at[i,'PMID'])
 
         db.session.add(ras_clus)
         db.session.commit()
 
-def create_search_dict(Clusters):
-    pdb_set=set();conformation_set=set();bound_protein_set=set();bound_protein_class_set=set();nucleotide_class_set=set();drug_class_set=set()
-    mutation_set=set()
+# def create_search_dict(Clusters):
+#     pdb_set=set();conformation_set=set();bound_protein_set=set();bound_protein_class_set=set();nucleotide_class_set=set();drug_class_set=set()
+#     mutation_set=set()
             
-    for items in Clusters.query.all():
-        pdb_set.add(items.pdb[:4])
-        conformation_set.add(items.conformational_state.strip())
-        nucleotide_class_set.add(items.nucleotide_class.strip())
-        drug_class_set.add(items.drug_class.strip())
-        bound_protein_class_set.add(items.bound_protein_class.strip())   #Check if these variables can also have ',' in them
+#     for items in Clusters.query.all():
+#         pdb_set.add(items.pdb[:4])
+#         conformation_set.add(items.conformational_state.strip())
+#         nucleotide_class_set.add(items.nucleotide_class.strip())
+#         drug_class_set.add(items.drug_class.strip())
+#         bound_protein_class_set.add(items.bound_protein_class.strip())   #Check if these variables can also have ',' in them
         
-        if ',' in items.bound_protein:
-            for protein in items.bound_protein.split(','):
-                bound_protein_set.add(protein.strip())
-        else:
-            bound_protein_set.add(items.bound_protein.strip())
+#         if ',' in items.bound_protein:
+#             for protein in items.bound_protein.split(','):
+#                 bound_protein_set.add(protein.strip())
+#         else:
+#             bound_protein_set.add(items.bound_protein.strip())
         
-        if ',' in items.mutation_status:
-            for mutation in items.mutation_status.split(','):
-                mutation_set.add(mutation.strip())
-        else:
-            mutation_set.add(items.mutation_status.strip())
+#         if ',' in items.mutation_status:
+#             for mutation in items.mutation_status.split(','):
+#                 mutation_set.add(mutation.strip())
+#         else:
+#             mutation_set.add(items.mutation_status.strip())
            
-    count_entire_set=dict()
-    for conf in conformation_set:
-        for mut in mutation_set:
-            for bp in bound_protein_set:
-                for bpc in bound_protein_class_set:
-                    for nucl in nucleotide_class_set:
-                        for drug in drug_class_set:
-                           if Clusters.query.filter(Clusters.conformational_state.contains(conf),Clusters.mutation_status.contains(mut),Clusters.bound_protein.contains(bp),Clusters.bound_protein_class.contains(bpc),Clusters.nucleotide_class.contains(nucl),Clusters.drug_class.contains(drug)).count()>0:
-                               count_entire_set[conf,mut,bp,bpc,nucl,drug]=Clusters.query.filter(Clusters.conformational_state.contains(conf),Clusters.mutation_status.contains(mut),Clusters.bound_protein.contains(bp),Clusters.bound_protein_class.contains(bpc),Clusters.nucleotide_class.contains(nucl),Clusters.drug_class.contains(drug)).count()
+#     count_entire_set=dict()
+#     for conf in conformation_set:
+#         for mut in mutation_set:
+#             for bp in bound_protein_set:
+#                 for bpc in bound_protein_class_set:
+#                     for nucl in nucleotide_class_set:
+#                         for drug in drug_class_set:
+#                            if Clusters.query.filter(Clusters.conformational_state.contains(conf),Clusters.mutation_status.contains(mut),Clusters.bound_protein.contains(bp),Clusters.bound_protein_class.contains(bpc),Clusters.nucleotide_class.contains(nucl),Clusters.drug_class.contains(drug)).count()>0:
+#                                count_entire_set[conf,mut,bp,bpc,nucl,drug]=Clusters.query.filter(Clusters.conformational_state.contains(conf),Clusters.mutation_status.contains(mut),Clusters.bound_protein.contains(bp),Clusters.bound_protein_class.contains(bpc),Clusters.nucleotide_class.contains(nucl),Clusters.drug_class.contains(drug)).count()
    
-    fhandle_pickle=open('search_dict.pkl','w')
-    pickle.dump(count_entire_set,fhandle_pickle)
-    fhandle_pickle.close()
+#     fhandle_pickle=open('search_dict.pkl','w')
+#     pickle.dump(count_entire_set,fhandle_pickle)
+#     fhandle_pickle.close()
     
 if __name__ == '__main__':
     filename=sys.argv[1]
     df=pd.read_csv(filename,sep='\t',header='infer')
     update_database(df)
-    create_search_dict(Clusters)
+    
